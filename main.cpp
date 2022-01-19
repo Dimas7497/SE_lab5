e07cabeeb29f40776f32268f13262293ac7ac0ab
Dimas Shamurin <shamurin97@mail.ru>
2022-01-19 23:45:29 +0500

    Деление чисел

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout << "Enter two integer numbers" << endl;
+    cout << "Enter two integer, confirm the inputs with Enter" << endl;
     cin >> a >> b;
     int c;
     c = a + b;
-    cout <<"a + b = " << c << endl;
+    cout <<"The sum of the entered numbers =" << c << endl;
     c = a - b;
-    cout <<"a - b = " << c << endl;
+    cout <<"The difference between the entered numbers =" << c << endl;
+    c = a / b;
+    cout << "The division the entered numbers =" << c << endl;
 }
